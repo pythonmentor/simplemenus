89df36238aa683463a48177502133c2cc7a9626f
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-06-18 10:18:20 +0200

    Update statemachine.py

--- simplemenus/statemachine.py
+++ simplemenus/statemachine.py
@@ -1,18 +1,16 @@
 """Module implémentant un exemple simpliste de machine à états."""
 
-from typing import Callable, Dict
-
 
 class SimpleStateMachine:
     """Représente une machine à états simple destinées à implémenter un
     parcours utilisateur dans un système de menus."""
 
-    def __init__(self, start_state: Callable) -> None:
+    def __init__(self, start_state):
         """Initialise une nouvelle machine à états."""
         self._next_state: Callable = start_state
 
-    def start(self) -> None:
+    def start(self):
         """Démarre la machine à états."""
-        args: Dict = {}
+        args = {}
         while self._next_state:
             self._next_state, args = self._next_state(**args)
